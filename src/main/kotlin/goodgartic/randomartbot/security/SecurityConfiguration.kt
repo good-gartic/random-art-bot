7840c71965e9c6357a93a319449e51dd426ca8de
Jiří Vrba <jiri@vrba.dev>
2021-10-07 22:06:42 +0200

    Add in-memory authentication with configured password

--- src/main/kotlin/goodgartic/randomartbot/security/SecurityConfiguration.kt
+++ src/main/kotlin/goodgartic/randomartbot/security/SecurityConfiguration.kt
@@ -1,13 +1,15 @@
 package goodgartic.randomartbot.security
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+class SecurityConfiguration(@Value("\${administration.password}") private val password: String) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http.cors()
@@ -21,4 +23,10 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 .authenticated()
     }
 
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+            .withUser("Tim")
+            .password(password)
+            .roles("USER")
+    }
 }
