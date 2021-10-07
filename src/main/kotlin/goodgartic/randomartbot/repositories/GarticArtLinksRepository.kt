c4d61283209ea5eef7508c2078d9924b22a4241c
Jiří Vrba <jiri@vrba.dev>
2021-10-07 23:14:59 +0200

    Work in progress error reporting implemented

--- src/main/kotlin/goodgartic/randomartbot/repositories/GarticArtLinksRepository.kt
+++ src/main/kotlin/goodgartic/randomartbot/repositories/GarticArtLinksRepository.kt
@@ -2,8 +2,9 @@ package goodgartic.randomartbot.repositories
 
 import goodgartic.randomartbot.entity.GarticArtLink
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface GarticArtLinksRepository : CrudRepository<GarticArtLink, UUID>
+interface GarticArtLinksRepository : CrudRepository<GarticArtLink, UUID>, PagingAndSortingRepository<GarticArtLink, UUID>
--- src/main/kotlin/goodgartic/randomartbot/service/GarticArtLinksService.kt
+++ src/main/kotlin/goodgartic/randomartbot/service/GarticArtLinksService.kt
@@ -1,10 +1,16 @@
 package goodgartic.randomartbot.service
 
 import goodgartic.randomartbot.configuration.DiscordConfiguration
+import goodgartic.randomartbot.entity.GarticArtLink
 import goodgartic.randomartbot.repositories.GarticArtLinksRepository
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.TextChannel
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
+import java.time.Instant
+import kotlin.random.Random
+import kotlin.math.max
 
 @Service
 class GarticArtLinksService(
@@ -19,6 +25,30 @@ class GarticArtLinksService(
         ?: throw IllegalStateException("Cannot find the configured repost channel (${configuration.repostChannelId})")
 
     fun repostRandomGarticArtLink() {
-       // TODO
+        val link = getRandomGarticArtLink() ?: return reportEmptyDatabase()
+    }
+
+    private fun getRandomGarticArtLink(): GarticArtLink? {
+        // Select a random row from 0 to N, where N is the number of all links in the database
+        val selected = Random.nextLong(max(repository.count(), 1)).toInt()
+        val pageable = PageRequest.of(selected, 1)
+
+        // The returned page will contain exactly 1 element or be empty.
+        // Empty page indicates, that the database contains no more links
+        return repository.findAll(pageable).firstOrNull()
+    }
+
+    private fun reportEmptyDatabase() {
+        val embed = EmbedBuilder()
+            .setTitle("Oh no!")
+            .setDescription("The gartic art links database returned no results!\nThis is probably a bug.")
+            .setColor(0xed4245)
+            .setTimestamp(Instant.now())
+            .build()
+
+        repostChannel
+            .sendMessage("Hey, ho <@${configuration.developerId}>!")
+            .setEmbeds(embed)
+            .queue()
     }
 }
