1f0b9fc1a5d2663e46a129ceecd11442dcea7098
Jiří Vrba <jiri@vrba.dev>
2021-10-07 23:58:18 +0200

    Work in progress JSON importing service

---
+++ src/main/kotlin/goodgartic/randomartbot/service/ImportService.kt
@@ -0,0 +1,28 @@
+package goodgartic.randomartbot.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import goodgartic.randomartbot.entity.GarticArtLink
+import goodgartic.randomartbot.repositories.GarticArtLinksRepository
+import org.springframework.stereotype.Service
+
+@Service
+class ImportService(private val repository: GarticArtLinksRepository) {
+
+    data class RawGarticArt(val image: String, val message: Long?)
+
+    fun processJsonBatch(json: String) {
+        val entries = extractGarticArtProperties(json)
+        val entities = entries.map { GarticArtLink(image = it.image, messageId = it.message, approved = false) }
+
+        repository.saveAll(entities)
+    }
+
+    private fun extractGarticArtProperties(json: String): List<RawGarticArt> {
+        val reader = ObjectMapper()
+        val source = reader.readTree(json) ?: throw IllegalArgumentException("Cannot parse the JSON tree")
+
+        TODO("Implement this")
+
+        return emptyList()
+    }
+}
