70f77c6d54daf76a65d95f5af1066802e0c145c6
Jiří Vrba <jiri@vrba.dev>
2021-10-07 17:03:25 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "random-art-bot"
---
+++ src/test/kotlin/goodgartic/randomartbot/RandomArtBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package goodgartic.randomartbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RandomArtBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
