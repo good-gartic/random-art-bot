9d07baef95eb21ef4775e4115b69be0e4bbc9545
Jiří Vrba <jiri@vrba.dev>
2021-10-07 22:01:26 +0200

    Add spring web + spring security dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
