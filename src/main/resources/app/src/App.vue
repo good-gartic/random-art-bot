535ac144eb4f94aa8c882db7e3a5e6fde1b86b56
Jiří Vrba <jiri@vrba.dev>
2021-10-08 15:31:16 +0200

    Add halfmoon css framework

--- src/main/resources/app/src/App.vue
+++ src/main/resources/app/src/App.vue
@@ -1,9 +1,15 @@
 <template>
+  <div>
+    <h1>Gartic art links management</h1>
+    <Import/>
+  </div>
 </template>
 
 <script>
 
+import Import from "@/components/Import";
 export default {
   name: 'App',
+  components: {Import},
 }
 </script>
---
+++ src/main/resources/app/src/components/Import.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <h3 class="card-title">Import links from JSON</h3>
+    <form @submit="upload()">
+      <div class="custom-file">
+        <input type="file" accept="application/json" required>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Import",
+  methods: {
+    upload(event) {
+      event.preventDefault()
+    }
+  }
+}
+</script>
